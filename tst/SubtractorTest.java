b57e4ae11ef470ad154e1a92ad14f16e30f3cd97
baranirajarathinam <baranirajarathinam@users.noreply.github.com>
2026-06-28 11:28:06 +0530

    Add unit test for Subtractor class

--- tst/SubtractorTest.java
+++ tst/SubtractorTest.java
@@ -1,1 +1,7 @@
-SubtractorTest.java
+ public class SubtractorTest {
+   public void testSub() {
+     Subtractor adder = new Subtractor();
+     int result = adder.sub(5,2);
+     assert(result==3);
+   }
+}
